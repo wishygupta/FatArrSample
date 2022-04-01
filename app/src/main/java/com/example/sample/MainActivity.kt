07a5cf2ceba222e4130b62a70bf2a4a30c1ef5c5
wishy <wishygupta@icloud.com>
2022-04-01 20:35:08 +0530

    sample app

---
+++ app/src/main/java/com/example/sample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lib1.Lib1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
